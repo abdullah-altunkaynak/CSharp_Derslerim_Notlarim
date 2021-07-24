236a13b0e98aa02c134d96409f82ca4f686b9e55
abdullah-altunkaynak <58576152+abdullah-altunkaynak@users.noreply.github.com>
2021-07-24 20:41:35 +0300

    Add files via upload

---
+++ Ders[9]/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿
+namespace Ders_9_
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPageUsers = new System.Windows.Forms.TabPage();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.tabPageCurrentUser = new System.Windows.Forms.TabPage();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.tabPageLocaleMachine = new System.Windows.Forms.TabPage();
+            this.listView3 = new System.Windows.Forms.ListView();
+            this.tabPageClassesRoot = new System.Windows.Forms.TabPage();
+            this.listView4 = new System.Windows.Forms.ListView();
+            this.tabPageCurrentConfig = new System.Windows.Forms.TabPage();
+            this.listView5 = new System.Windows.Forms.ListView();
+            this.tabControl1.SuspendLayout();
+            this.tabPageUsers.SuspendLayout();
+            this.tabPageCurrentUser.SuspendLayout();
+            this.tabPageLocaleMachine.SuspendLayout();
+            this.tabPageClassesRoot.SuspendLayout();
+            this.tabPageCurrentConfig.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(204, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(298, 27);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ALL KEYS AND SUBKEYS";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPageUsers);
+            this.tabControl1.Controls.Add(this.tabPageCurrentUser);
+            this.tabControl1.Controls.Add(this.tabPageLocaleMachine);
+            this.tabControl1.Controls.Add(this.tabPageClassesRoot);
+            this.tabControl1.Controls.Add(this.tabPageCurrentConfig);
+            this.tabControl1.Location = new System.Drawing.Point(12, 61);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(776, 377);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // tabPageUsers
+            // 
+            this.tabPageUsers.Controls.Add(this.listView1);
+            this.tabPageUsers.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.tabPageUsers.Location = new System.Drawing.Point(4, 29);
+            this.tabPageUsers.Name = "tabPageUsers";
+            this.tabPageUsers.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageUsers.Size = new System.Drawing.Size(768, 344);
+            this.tabPageUsers.TabIndex = 0;
+            this.tabPageUsers.Text = "HKEY_USERS";
+            this.tabPageUsers.UseVisualStyleBackColor = true;
+            // 
+            // listView1
+            // 
+            this.listView1.Font = new System.Drawing.Font("Arial", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(27, 16);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(722, 305);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            // 
+            // tabPageCurrentUser
+            // 
+            this.tabPageCurrentUser.Controls.Add(this.listView2);
+            this.tabPageCurrentUser.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.tabPageCurrentUser.Location = new System.Drawing.Point(4, 29);
+            this.tabPageCurrentUser.Name = "tabPageCurrentUser";
+            this.tabPageCurrentUser.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageCurrentUser.Size = new System.Drawing.Size(768, 344);
+            this.tabPageCurrentUser.TabIndex = 1;
+            this.tabPageCurrentUser.Text = "HKEY_CURRENT_USER";
+            this.tabPageCurrentUser.UseVisualStyleBackColor = true;
+            // 
+            // listView2
+            // 
+            this.listView2.Font = new System.Drawing.Font("Arial", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.listView2.HideSelection = false;
+            this.listView2.Location = new System.Drawing.Point(32, 29);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(713, 297);
+            this.listView2.TabIndex = 0;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.View = System.Windows.Forms.View.List;
+            // 
+            // tabPageLocaleMachine
+            // 
+            this.tabPageLocaleMachine.Controls.Add(this.listView3);
+            this.tabPageLocaleMachine.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.tabPageLocaleMachine.Location = new System.Drawing.Point(4, 29);
+            this.tabPageLocaleMachine.Name = "tabPageLocaleMachine";
+            this.tabPageLocaleMachine.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageLocaleMachine.Size = new System.Drawing.Size(768, 344);
+            this.tabPageLocaleMachine.TabIndex = 2;
+            this.tabPageLocaleMachine.Text = "HKEY_LOCAL_MACHİNE";
+            this.tabPageLocaleMachine.UseVisualStyleBackColor = true;
+            // 
+            // listView3
+            // 
+            this.listView3.Font = new System.Drawing.Font("Arial", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.listView3.HideSelection = false;
+            this.listView3.Location = new System.Drawing.Point(50, 40);
+            this.listView3.Name = "listView3";
+            this.listView3.Size = new System.Drawing.Size(691, 285);
+            this.listView3.TabIndex = 0;
+            this.listView3.UseCompatibleStateImageBehavior = false;
+            this.listView3.View = System.Windows.Forms.View.List;
+            // 
+            // tabPageClassesRoot
+            // 
+            this.tabPageClassesRoot.Controls.Add(this.listView4);
+            this.tabPageClassesRoot.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.tabPageClassesRoot.Location = new System.Drawing.Point(4, 29);
+            this.tabPageClassesRoot.Name = "tabPageClassesRoot";
+            this.tabPageClassesRoot.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageClassesRoot.Size = new System.Drawing.Size(768, 344);
+            this.tabPageClassesRoot.TabIndex = 3;
+            this.tabPageClassesRoot.Text = "HKEY_CLASSES_ROOT";
+            this.tabPageClassesRoot.UseVisualStyleBackColor = true;
+            // 
+            // listView4
+            // 
+            this.listView4.Font = new System.Drawing.Font("Arial", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.listView4.HideSelection = false;
+            this.listView4.Location = new System.Drawing.Point(26, 22);
+            this.listView4.Name = "listView4";
+            this.listView4.Size = new System.Drawing.Size(716, 300);
+            this.listView4.TabIndex = 0;
+            this.listView4.UseCompatibleStateImageBehavior = false;
+            this.listView4.View = System.Windows.Forms.View.List;
+            // 
+            // tabPageCurrentConfig
+            // 
+            this.tabPageCurrentConfig.Controls.Add(this.listView5);
+            this.tabPageCurrentConfig.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.tabPageCurrentConfig.Location = new System.Drawing.Point(4, 29);
+            this.tabPageCurrentConfig.Name = "tabPageCurrentConfig";
+            this.tabPageCurrentConfig.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageCurrentConfig.Size = new System.Drawing.Size(768, 344);
+            this.tabPageCurrentConfig.TabIndex = 4;
+            this.tabPageCurrentConfig.Text = "HKEY_CURRENT_CONFİG";
+            this.tabPageCurrentConfig.UseVisualStyleBackColor = true;
+            // 
+            // listView5
+            // 
+            this.listView5.Font = new System.Drawing.Font("Arial", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.listView5.HideSelection = false;
+            this.listView5.Location = new System.Drawing.Point(17, 24);
+            this.listView5.Name = "listView5";
+            this.listView5.Size = new System.Drawing.Size(734, 317);
+            this.listView5.TabIndex = 0;
+            this.listView5.UseCompatibleStateImageBehavior = false;
+            this.listView5.View = System.Windows.Forms.View.List;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPageUsers.ResumeLayout(false);
+            this.tabPageCurrentUser.ResumeLayout(false);
+            this.tabPageLocaleMachine.ResumeLayout(false);
+            this.tabPageClassesRoot.ResumeLayout(false);
+            this.tabPageCurrentConfig.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPageUsers;
+        private System.Windows.Forms.TabPage tabPageCurrentUser;
+        private System.Windows.Forms.TabPage tabPageLocaleMachine;
+        private System.Windows.Forms.TabPage tabPageClassesRoot;
+        private System.Windows.Forms.TabPage tabPageCurrentConfig;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.ListView listView3;
+        private System.Windows.Forms.ListView listView4;
+        private System.Windows.Forms.ListView listView5;
+    }
+}
+
---
+++ Ders[9]/Form1.cs
@@ -0,0 +1,175 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+#region Registry Procceses - Kayıt Defteri İşlemleri
+
+#region Tanım
+/*
+    Registery, işletim sistemi ve bu işletim sistemi içerisinde yüklü olan programlar, kullanıcı bilgileri gibi bilgilerin tutulduğu
+kayıt defteridir. Biz de geliştirdiğimiz programımız ile ilgili veya kullanıcılar ile ilgili kısa bilgileri saklamak istediğimizde
+registery kullanacağız.
+*/
+#endregion
+
+#region Registry Root/Main Keys - Kayıt Defteri Kök/Ana Anahtarlar
+/*
+    Registry içerisinde varsayılan olarak var olan HKEY_CLASSES_ROOT, HKEY_CURRENT_USER, HKEY_LOCAL_MACHINE, HKEY_USERS ve
+HKEY_CURRENT_CONFIG olmak üzere 5 adet anahtar bulunur.
+    # HKEY_CLASSES_ROOT: Windows işletim sistemi ve çalışan procces'ler ile ilgili bilgileri tutar.
+    # HKEY_CURRENT_USER: Geçerli kullanıcı hakkındaki bilgileri ve geçerli ayarları saklar.
+    # HKEY_LOCAL_MACHINE: Bilgisayarın konfigürasyon bilgileri saklanır.
+    # HKEY_USERS: İşletim sisteminde var olan tüm kullanıcılar hakkında bilgileri ve geçerli ayarları saklar.
+    # HKEY_CURRENT_CONGIG: Donanım bilgilerini saklar.
+    Registry üzerinde işlem yapabilmek için "Microsoft.Win32" sınıfını projemize dahil etmemiz gerekmektedir. Ayrıca registery
+işlemlerinde kullanacağımız keyleri "RegistryKey" sınıfından türetilmiş nesneler ile tanımlayacağız.
+*/
+#endregion
+
+#region Accessing Registry Key & Registry Value - Registry Anahtarlarına & Registry Değerlerine Erişim
+/*
+    # Registry'deki anahtarlara erişebilmek için OpenSubKey() metodu kullanılır.
+    # Registry'deki değerlere erişebilmek için GetValue() metodu kullanılır.
+Not: Example Of Accessing Registery Key & Value region alanındaki örneği inceleyiniz. 
+*/
+#endregion
+
+#region Adding Key & Value to Registry - Kayıt Defterine Anahtar ve Değer Ekleme
+/*
+    # Registry'e yeni bir anahtar oluşturmak için CreateSubKey metodu kullanılır.
+    # Registry'e yeni bir değer eklemek için SetValue metodu kullanılır.
+    Registry'e yeni bir değer eklerken değer türü yani "RegistryValueKind parametresi aşağıdaki değerleri alabilir. Bu değerler
+registry'e ekleyebileceğimiz her türlü veri tipidir.
+    #1. String: Katar/Dize Değeri.
+    #2. Binary: İkilik Değer.
+    #3. DWord: 32 bit Binary Değer.
+    #4. MultiString: Çok Katar/Dizeli Değer.
+    #4. QWord: 64 bit Binary Değer.
+    #5. Unknow: Genişletilebilir Katar/Dize Değeri.
+    Registry'e değer eklerken varsayılan keyler için iznimiz olmayabilir. Örneğin KHEY_LOCAL_MACHINE yönetici iznine ihtiyaç 
+duyarken HKEY_CURRENT_USER hiç bir izne ihtiyaç duymaz.
+NOT: Example Of Adding Key & Value to Registry region alanındaki örneği inceleyiniz.
+*/
+#endregion
+
+#endregion
+
+#region Homework - Ödev
+/*
+    1-)Registry Anahtar ve Değer sayısını bulma, Anahtar ve Değer listesini alma, Anahtar ve Değer silme ve RegistryKeyPermissionCheck
+OpenSubKey metodundaki parametresinin alabileceği değerleri araştır.
+*/
+#endregion
+
+namespace Ders_9_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            #region Example Of Accessing Registery Key & Value Apply
+            /*RegisteryKeyAndValue();*/
+            #endregion
+
+            #region Example Of Adding Key & Value to Registry Apply
+            /*AddingKeyAndValueToRegistry();*/
+            #endregion
+
+            #region Solution of Homework Apply
+            ArrayList liste = new ArrayList();           
+            PrintKeys(Registry.Users, liste, listView1);          
+            PrintKeys(Registry.CurrentUser, liste, listView2);                  
+            PrintKeys(Registry.LocalMachine, liste, listView3);            
+            PrintKeys(Registry.ClassesRoot, liste, listView4);            
+            PrintKeys(Registry.CurrentConfig, liste, listView5);
+            #endregion
+        }
+
+        #region Example Of Accessing Registery Key & Value
+        /*
+            Internet Explorer versiyonunu kayıt defterinden çeken kodu yazıyoruz. Internet Explorer versiyonu kayıt defteri üzerinde
+        HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Internet Explorer yolunda bulunur.
+         */
+        public void RegisteryKeyAndValue()
+        {
+            try
+            {
+                RegistryKey key = Registry.LocalMachine.OpenSubKey("SOFTWARE\\Microsoft\\Internet Explorer", false);
+                string value = key.GetValue("Version").ToString();
+                MessageBox.Show(value, "Internet Explorer Version!");
+            }
+            catch
+            {
+                MessageBox.Show("Internet Explorer not Installed in this Computer!", "Warning!");
+            }
+        }
+        #endregion
+
+        #region Example Of Adding Key & Value to Registry
+        /*
+            HKEY_CURRENT_USER\Software key altına ExampleOfRegistry isminde yeni bir key oluşturarak, bu key içerisinde SomeValue
+        isimli bir değer oluşturacağız.
+        */
+        public void AddingKeyAndValueToRegistry()
+        {
+            try
+            {
+                RegistryKey key = Registry.CurrentUser.OpenSubKey("Software", true);
+                RegistryKey newKey = key.CreateSubKey("ExampleOfRegistry");
+                newKey.SetValue("SomeValue", "978");
+                MessageBox.Show("Registry Key & Value Successfully Created!", "Successful!");
+            }
+            catch
+            {
+                MessageBox.Show("Registry Key & Value Cant Created!", "Unsuccessful!");
+            }
+        }
+        #endregion
+
+        #region Solution of Homework
+        /*   Registry Main anahtarlarının alt anahtarlarının alınması ve ekrana yazdırılması. */
+        static void PrintKeys(RegistryKey rkey, ArrayList liste, ListView listview)
+        {           
+            liste.AddRange(rkey.GetSubKeyNames());
+            foreach (string i in liste)
+                listview.Items.Add(i);
+            liste.Clear();           
+        }
+        #region Registry Fields
+        /*
+            ClassesRoot	: Belgelerin türlerini (veya sınıflarını) ve bu türlerle ilişkili özellikleri tanımlar. Bu alan Windows kayıt defteri
+        temel anahtarını HKEY_CLASSES_ROOT okur.
+            CurrentConfig : Kullanıcıya özgü olmayan donanımla ilgili yapılandırma bilgilerini içerir. Bu alan Windows kayıt defteri temel
+        anahtarını HKEY_CURRENT_CONFIG okur.
+            CurrentUser : Geçerli Kullanıcı tercihleri hakkında bilgi içerir. Bu alan Windows kayıt defteri temel anahtarını HKEY_CURRENT_USER okur.
+            LocalMachine : Yerel makine için yapılandırma verilerini içerir. Bu alan Windows kayıt defteri temel anahtarını HKEY_LOCAL_MACHINE okur.
+            PerformanceData : Yazılım bileşenleri için performans bilgilerini içerir. Bu alan Windows kayıt defteri temel anahtarını
+        HKEY_PERFORMANCE_DATA
+        okur.
+            Users : Varsayılan Kullanıcı Yapılandırması hakkında bilgi içerir. Bu alan Windows kayıt defteri temel anahtarını HKEY_USERS okur.
+         */
+        #endregion
+        #region Registry Properties
+        /*
+            GetValue(String, String, Object) : Belirtilen kayıt defteri anahtarındaki belirtilen adla ilişkili değeri alır. Ad belirtilen anahtarda
+        bulunamazsa, sağladığınız varsayılan değeri veya null belirtilen anahtar yoksa, döndürür.
+            SetValue(String, String, Object) : Belirtilen kayıt defteri anahtarında belirtilen ad/değer çiftini ayarlar. Belirtilen anahtar yoksa,
+        oluşturulur.
+            SetValue(String, String, Object, RegistryValueKind) : Belirtilen kayıt defteri veri türünü kullanarak belirtilen kayıt defteri anahtarındaki
+        ad/değer çiftini ayarlar. Belirtilen anahtar yoksa, oluşturulur.
+        Registry.CurrentUser.SetValue("Kullanici", "ASUS"); küçük bir örnek.
+         */
+        #endregion
+        #endregion
+    }
+}
